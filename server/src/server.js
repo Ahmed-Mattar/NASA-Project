1f1a7fd4db77b7f05089e485b12b7c63a8f1f7ce
Ahmed Mattar <AceMattar@gmail.com>
2022-03-31 06:40:53 +0200

    feat: load data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,19 @@
-const http = require('http')
-const app = require('./app')
+const http = require("http");
+const app = require("./app");
 
+const { loadPlanetData } = require("./models/planets.models");
 
-
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 // his makes it easy to provide both HTTP and HTTPS versions of your app with the same code base
 // https.createServer(options, app).listen(443
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}...`)
-})
+async function startServer() {
+  await loadPlanetData();
 
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+startServer();
