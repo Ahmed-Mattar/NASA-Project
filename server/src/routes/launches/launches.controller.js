3d8670ff1679437cbaeb389e87ab89b0d80a4b74
Ahmed Mattar <AceMattar@gmail.com>
2022-04-09 15:51:46 +0200

    feat:connect fron POST/launches with fron end dashboard

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,23 +1,36 @@
-const {getAllLaunches, addNewLaunch} = require('../../models/launches.model')
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
-function HttpGetAllLaunches(req,res) {
-    
-    return res.status(200).json(getAllLaunches());
+function HttpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches());
 }
 
-function HttpAddNewLaunch(req,res) {
+function HttpAddNewLaunch(req, res) {
+  const launch = req.body;
 
-    const launch = req.body
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
 
-    launch.launchDate = new Date(launch.launchDate)
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
 
-    addNewLaunch(launch)
+  addNewLaunch(launch);
 
-    return res.status(201).json(launch)
+  return res.status(201).json(launch);
 }
-
 
 module.exports = {
-    HttpGetAllLaunches,
-    HttpAddNewLaunch
-}
+  HttpGetAllLaunches,
+  HttpAddNewLaunch,
+};
