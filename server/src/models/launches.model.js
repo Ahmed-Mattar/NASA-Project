70a02d26dddfa3a85de9a6267605eb31cf67cefc
Ahmed Mattar <AceMattar@gmail.com>
2022-04-24 03:20:56 +0200

    feat: using axios to send requests to spaceX api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,20 +1,47 @@
+const axios = require("axios");
+
 const launches = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
+  flightNumber: 100, // flight_number       spaceX
+  mission: "Kepler Exploration X", // name   spaceX
+  rocket: "Explorer IS1", // rocket.name     spaceX
+  launchDate: new Date("December 27, 2030"), // date_local  spaceX
+  target: "Kepler-442 b", // not applicable
+  customers: ["ZTM", "NASA"], // payload.customer for each payload spaceX
+  upcoming: true, // upcoming   spaceX
+  success: true, // success     spaceX
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function loadLaunchesData() {
+  console.log("Downloading launch data");
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+}
 
 async function existsLaunchWithId(launchId) {
   return await launches.findOne({
@@ -83,6 +110,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+  loadLaunchesData,
   existsLaunchWithId,
   getAllLaunches,
   scheduleNewLaunch,
--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetData } = require("./models/planets.model");
-
+const { loadLaunchData } = require("./models/launches.model");
 const PORT = process.env.PORT || 8000;
 
 // his makes it easy to provide both HTTP and HTTPS versions of your app with the same code base
@@ -13,6 +13,8 @@ const server = http.createServer(app);
 
 async function startServer() {
   await mongoConnect();
+  await loadPlanetData();
+  await loadLaunchData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
